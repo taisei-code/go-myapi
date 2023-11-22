f714ad88be49063fc6991e411f3c07982ab9e1ac
Taisei <taisei96.web@gmail.com>
2023-11-23 08:10:20 +0900

    ハンドラ追加

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"io"
 	"log"
 	"net/http"
@@ -17,10 +18,33 @@ func main () {
 		io.WriteString(w, "Posting Article...\n")
 	}
 
+	articleListHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Article List\n")
+	}
+
+	articleDetailHandler := func(w http.ResponseWriter, req *http.Request) {
+		articleID := 1
+		resString := fmt.Sprintf("Article No.%d\n", articleID)
+		io.WriteString(w, resString)
+	}
+
+	postNiceHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Nice...\n")
+	}
+
+	postCommentHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Posting Comment...\n")
+	}
+
+
 	// 定義したハンドラをサーバーで使う登録
-	// http.HandleFunc("/", helloHandler)
 	http.HandleFunc("/hello", helloHandler)
 	http.HandleFunc("/article", postArticleHandler)
+	http.HandleFunc("/article/list", articleListHandler)
+	http.HandleFunc("/article/1", articleDetailHandler)
+	http.HandleFunc("/article/nice", postNiceHandler)
+	http.HandleFunc("/comment", postCommentHandler)
+
 
 	// サーバー起動時のログ出力
 	log.Println("server start at port 8080")
