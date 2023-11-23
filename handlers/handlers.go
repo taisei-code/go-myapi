46d4ae18e0a6e3caa65679ff5070b47890ba85f2
Taisei <taisei96.web@gmail.com>
2023-11-23 09:58:09 +0900

    gorilla/muxのルータ導入

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -8,35 +8,68 @@ import (
 
 // helloのハンドラ
 func HelloHandler(w http.ResponseWriter, req *http.Request) {
-		// ハンドラの処理内容（何がきても、Hello, Worldを返す
+
+	//reqのMethodフィールドがGETなら
+	if req.Method == http.MethodGet {
+		// 通常通りにレスポンスを返す
 		io.WriteString(w, "Hello, world!\n")
+
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
+
 }
 
 // articleのハンドラ
 func PostArticleHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
 		io.WriteString(w, "Posting Article...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/listのハンドラ
 func ArticleListHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
 		io.WriteString(w, "Article List\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/1のハンドラ
 func ArticleDetailHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodGet {
 		articleID := 1
 		resString := fmt.Sprintf("Article No.%d\n", articleID)
 		io.WriteString(w, resString)
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 // /article/niceのハンドラ
 func PostNiceHandler(w http.ResponseWriter, req *http.Request) {
-		io.WriteString(w, "Posting Nice...\n")
+
+		if req.Method == http.MethodPost {
+			io.WriteString(w, "Posting Nice...\n")
+		} else {
+			http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+		}
 }
 
 // /commentのハンドラ
 func  PostCommentHandler(w http.ResponseWriter, req *http.Request) {
+
+	if req.Method == http.MethodPost {
 		io.WriteString(w, "Posting Comment...\n")
+	} else {
+		http.Error(w, "Invalid method", http.StatusMethodNotAllowed)
+	}
 }
 
 
--- main.go
+++ main.go
@@ -4,23 +4,26 @@ import (
 	"log"
 	"net/http"
 
+	"github.com/gorilla/mux"
 	"github.com/yourname/reponame/handlers"
 )
 
 func main () {
-	
+	// ルータrを明示的に宣言
+	r := mux.NewRouter()
+
 	// 定義したハンドラをサーバーで使う登録
-	http.HandleFunc("/hello", handlers.HelloHandler)
-	http.HandleFunc("/article", handlers.PostArticleHandler)
-	http.HandleFunc("/article/list", handlers.ArticleListHandler)
-	http.HandleFunc("/article/1", handlers.ArticleDetailHandler)
-	http.HandleFunc("/article/nice", handlers.PostNiceHandler)
-	http.HandleFunc("/comment", handlers.PostCommentHandler)
+	r.HandleFunc("/hello", handlers.HelloHandler)
+	r.HandleFunc("/article", handlers.PostArticleHandler)
+	r.HandleFunc("/article/list", handlers.ArticleListHandler)
+	r.HandleFunc("/article/1", handlers.ArticleDetailHandler)
+	r.HandleFunc("/article/nice", handlers.PostNiceHandler)
+	r.HandleFunc("/comment", handlers.PostCommentHandler)
 
 
 	// サーバー起動時のログ出力
 	log.Println("server start at port 8080")
 
 	// ListenAndServe関数でサーバー起動
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
