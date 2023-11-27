c2dfd9d11da1162e3656fae2a2053fd7122ef1d3
Taisei <taisei96.web@gmail.com>
2023-11-28 06:50:56 +0900

    go構造体をエンコード

--- main.go
+++ main.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 	"github.com/yourname/reponame/handlers"
 )
+
+type Comment struct {
+	CommentID int
+	ArticleID int
+	Message   string
+	CreatedAt time.Time
+}
+
+type Article struct {
+	ID          int
+	Title       string
+	Contents    string
+	UserName    string
+	NiceNum     int
+	CommentList []Comment
+	CreatedAt   time.Time
+}
 
 func main () {
 	// ルータrを明示的に宣言
@@ -20,10 +40,41 @@ func main () {
 	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
 	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
 
+	comment1 := Comment{
+		CommentID: 1,
+		ArticleID: 1,
+		Message:   "test comment1",
+		CreatedAt: time.Now(),
+	}
+
+	comment2 := Comment{
+		CommentID: 2,
+		ArticleID: 1,
+		Message:   "second comment",
+		CreatedAt: time.Now(),
+	}
+	article := Article{
+		ID:         1,
+		Title:      "first article",
+		Contents:   "This is the test article",
+		UserName:   "saki",
+		NiceNum:     1,
+		CommentList: []Comment{comment1, comment2},
+		CreatedAt:   time.Now(),
+	}
+
+	jsonData, err := json.Marshal(article)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Printf("%s\n", jsonData)
 
 	// サーバー起動時のログ出力
 	log.Println("server start at port 8080")
 
 	// ListenAndServe関数でサーバー起動
 	log.Fatal(http.ListenAndServe(":8080", r))
+
 }
+
